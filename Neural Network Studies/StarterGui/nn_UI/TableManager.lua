29e161cae0ea8c8ae159db7a95180f8830b7c4da
Richard Scott <82393276+Bububooo3@users.noreply.github.com>
2025-03-31 21:44:12 +0000

    Updated system

--- Neural Network Studies/StarterGui/nn_UI/TableManager.lua
+++ Neural Network Studies/StarterGui/nn_UI/TableManager.lua
@@ -1,14 +1,61 @@
+--// CONSTANTS
+local START_VALUE = {0}
+
+--// USEFUL FUNCTIONS
+local function createGrid(size)
+  local grid = {}
+
+  for iterator_x, size.X do
+    grid[iterator_x] = {}
+
+    if size.Y > 0 then
+      for iterator_y, size.Y do
+        grid[iterator_x][1] = iterator_y
+      end
+    end
+
+    if size.Z > 0 then
+      for iterator_z, size.Z do
+        grid[iterator_x][2] = iterator_z
+      end
+    end
+
+  end
+
+  --[[
+  Returns:
+  grid = {
+  [x] = {y,z},
+  [x] = {y,z},
+  [x] = {y,z}
+  }
+  ]]
+
+  return grid
+end
+
+--// MODULE
 local _table = {}
 
+--// CREATE NEW GRID (1D, 2D, 3D)
 function _table.NewGrid(x, y, z)
-  local grid: {number}
+  if not(x) or x < 0 then error("Invalid x parameter: "..x) end
 
-  grid.X, grid.Y, grid.Z = x or -1, y or -1, z or -1
+  local self = {}
 
-  if grid.X >= 0 then
-    grid = table.create()
+  self.Size = {
+    ["X"] = x or -1,
+    ["Y"] = y or -1,
+    ["Z"] = z or -1
+  }
+
+  if self.Size >= 0 then
+    self.Grid = createGrid(self.Size)
+    
+
   end
   
+  return self
 end
 
 
